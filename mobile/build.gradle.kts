e7fbae870390596d70d247bf8594c37a08ec472c
shsim23 <shsim23@kaist.ac.kr>
2023-10-29 01:40:29 +0900

    message sent

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.wear_study"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.wear_study"
@@ -30,6 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -40,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.gms:play-services-wearable:18.0.0")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.android.tools.build:aapt2:8.1.2-10154469")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- mobile/src/main/java/com/example/wear_study/MainActivity.kt
+++ mobile/src/main/java/com/example/wear_study/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.wear_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent.DispatcherState
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.CapabilityInfo
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+private const val DUAL_VIEWER_WATCH_CAPABILITY_NAME = "dual_viewer_watch"
+private const val DUAL_VIEWER_PATH = "/dual_viewer"
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+    private var num = 0
+    private lateinit var txtNum: TextView
+    private val mClient by lazy { Wearable.getMessageClient(this)}
+    private val cClient by lazy { Wearable.getCapabilityClient(this)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bPlus = findViewById<Button>(R.id.bUp)
+        val bMinus = findViewById<Button>(R.id.bDown)
+        txtNum = findViewById<TextView>(R.id.num)
+
+        bPlus.setOnClickListener(this)
+        bMinus.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.bUp -> num++
+            R.id.bDown -> num--
+        }
+        txtNum.text = "number : ${num.toString()}"
+        sendMsg()
+    }
+
+    private fun sendMsg(){
+        CoroutineScope(Dispatchers.IO).launch {
+            val nodes = Tasks.await(
+                cClient.getCapability(
+                    DUAL_VIEWER_WATCH_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE
+                )
+            ).nodes
+            var nodeId = nodes.first().id
+            mClient
+                .sendMessage(nodeId, DUAL_VIEWER_PATH, byteArrayOf(num.toByte()))
+        }
     }
 }
+
